1a68554d45dcca0c1adce1ddfba7f7609968f256
fathifathan <fathifathan30@gmail.com>
2023-02-28 21:33:28 +0700

    commit 1

---
+++ js/halaman_juz/script8.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+
+    $(".radio-btn").click(function () {
+        $(".radio-inner").toggleClass("active");
+        $("body").toggleClass("dark");
+    });
+
+    $('.buttonHide').click(function () {
+
+        $('#gambar1').toggle()
+        const tombol = $(this).text()
+
+        if (tombol == 'Hide') {
+            $(this).text('Show')
+        }
+
+        if (tombol == 'Show') {
+            $(this).text('Hide')
+        }
+        console.log($(this).text())
+    })
+
+
+    $('.buttonNext').click(function () {
+        const gambar = $('#gambar1').attr('src');
+        const split = gambar.split('/')[5]
+        const nomor = parseInt(split.substring(0, split.indexOf('.'))) + 1
+
+        if (nomor <= 161) {
+            const url = '../../img/image_juz/juz8/' + nomor + '.png'
+            $('#gambar1').attr('src', url);
+        }
+    })
+
+    $('.buttonBack').click(function () {
+        const gambar = $('#gambar1').attr('src');
+        const split = gambar.split('/')[5]
+        const nomor = parseInt(split.substring(0, split.indexOf('.'))) - 1
+
+        if (nomor >= 142) {
+            const url = '../../img/image_juz/juz8/' + nomor + '.png'
+            $('#gambar1').attr('src', url);
+        }
+
+
+    })
+});
